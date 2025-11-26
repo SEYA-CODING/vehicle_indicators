2c412e49b6422f793a131cb32f45f8993edd30e4
SEYA-CODING <seyattv@gmail.com>
2025-11-26 04:45:04 +0000

    Create config.lua for vehicle indicators
    Add configuration for vehicle indicators including keybinds.

---
+++ config.lua
@@ -0,0 +1,17 @@
+-- vehicle_indicators v1.1.0 by SEYA CODING
+-- Discord: seya_coding #6497
+-- Community: https://discord.gg/RT3uJRdXS
+
+Config = {}
+
+-- Keybinds:
+-- LEFT arrow = 174
+-- RIGHT arrow = 175
+-- BACKSPACE = 202
+-- E = 38 (Flash/high beam)
+-- More key codes here: https://docs.fivem.net/docs/game-references/controls/
+
+Config.LeftSignalKey  = 174   -- Left Arrow
+Config.RightSignalKey = 175   -- Right Arrow
+Config.HazardKey      = 202   -- Backspace
+Config.FlashKey       = 38    -- E Key
