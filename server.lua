16dcdc3d7eb7d868fee700d3449665b196c03f65
SEYA-CODING <seyattv@gmail.com>
2025-11-26 04:46:56 +0000

    Add vehicle_indicators v1.1.0 initialization messages

---
+++ server.lua
@@ -0,0 +1,10 @@
+-- vehicle_indicators v1.1.0 by SEYA CODING
+-- Discord: seya_coding #6497
+-- Community: https://discord.gg/RT3uJRdXS
+
+print("^2==============================================^0")
+print("^3  vehicle_indicators v1.1.0 Loaded Successfully!^0")
+print("^6  Created by SEYA CODING                      ^0")
+print("^6  Discord: seya_coding #6497                  ^0")
+print("^6  Community: https://discord.gg/RT3uJRdXS     ^0")
+print("^2==============================================^0")
