86ce33fc6f96d75b80b90efb458384e3881e8ff7
SEYA-CODING <seyattv@gmail.com>
2025-11-26 04:44:26 +0000

    Create fxmanifest.lua for vehicle indicators
    Add fxmanifest for vehicle indicators resource

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+-- vehicle_indicators v1.1.0 by SEYA CODING
+-- Discord: seya_coding #6497
+-- Community: https://discord.gg/RT3uJRdXS
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SEYA CODING'
+description 'Vehicle Indicators / Blinkers / Hazards / Fullbeam'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
